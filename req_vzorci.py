91a99745be174b39c86cbcbb410d27d455c45149
avbreht123 <ziga.avbreht@gmail.com>
2019-11-05 17:13:28 +0100

    html zloudan strani s poskusi napisati uspesni regex za ujemanje

---
+++ req_vzorci.py
@@ -0,0 +1,29 @@
+import re
+import test
+
+
+with open('html_koda.html', 'r') as datoteka:
+    vsebina = datoteka.read()
+
+
+vzorec_bloka = re.compile(
+    r"<td class='collection_rank' align='center' >.*?"
+    r"<td class='collection_shop'>",
+    flags=re.DOTALL
+)
+
+vzorec_igre = re.compile(
+    r'<a   href="/boardgame/\d/\D" >',
+    flags=re.DOTALL
+)
+
+vzorec_ocene = re.compile(
+    r"<td class='collection_bggrating' align='center'>.*?",
+    flags=re.DOTALL
+)
+
+count = 0
+for ujemanje in re.finditer(vzorec_ocene, vsebina):
+    print(ujemanje.groupdict())
+    count += 1
+print(count)
